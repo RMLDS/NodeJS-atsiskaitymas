c7fbb5b565faad83b3dc8f64ae27a70b0d829b09
RMLDS <romualdas.bukauskas@live.com>
2022-05-24 13:44:45 +0300

    added db connection, validation, registration

---
+++ middleware/validation.js
@@ -0,0 +1,15 @@
+import joi from 'joi';
+
+// registration validation
+export const schema = joi.object({
+    username: joi.string().trim().required(),
+    email: joi.string().email().trim().lowercase().required(),
+    password: joi.string().min(5).required(),
+    password_repeat: joi.ref('password')
+});
+
+// login validation
+export const login_schema = joi.object({
+    email: joi.string().email().trim().lowercase().required(),
+    password: joi.string().min(5).required()
+});
--- routes/ui/register.js
+++ routes/ui/register.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import con from '../../dbConnection.js';
+import {schema} from '../../middleware/validation.js';
 
 const router = express.Router();
 
@@ -10,6 +13,32 @@ router.get('/', (req, res) => {
         }
     catch (error) {
         console.log(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = req.body;
+        const { error } = schema.validate(userData);
+        if (error) return res.status(400).send(`Error: ${error.details[0].message}`);
+
+        let [data] = await con.query(`SELECT * FROM user WHERE email = ?`, [userData.email]);
+        if (data.length !== 0) return res.status(400).send(`Email already exists.`);
+        [data] = await con.query(`SELECT * FROM user WHERE name = ?`, [userData.username]);
+        if (data.length !== 0) return res.status(400).send(`Username alrready taken.`);
+
+        const hashedPassword = bcrypt.hashSync(userData.password, parseInt(process.env.SALT));
+        await con.query(`INSERT INTO user SET ?
+        `, {
+            name: userData.username,
+            email: userData.email,
+            password: hashedPassword
+        });
+        //await con.end();
+        res.send(`Created new user: ${userData.email}`);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        return res.status(400).send('Incorrect data sent.');
     }
 });
 
